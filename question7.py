13551c380caffccd27a1a09bade44e59f9138170
imku13 <imregulkurt@gmail.com>
2025-05-17 11:27:57 +0300

    minor comment removal

--- question7.py
+++ question7.py
@@ -1,4 +1,3 @@
-#Kullancidan pozitif bir sayi bekleyen, pozitifi de gördügü an bastiran, negatif sayi girildikçe bir daha soran yapi kuralim. (for döngüsü ile)
 for _ in range(1000):  # Yüksek sayıda tekrar hakkı (örnek olarak 1000)
     value = int(input("Pozitif bir sayı girin: "))
     if value > 0:
